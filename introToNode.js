197286bfdcf574e60c598acc80e13fbbdf077f70
Regie Marzan <lonebeginner@gmail.com>
2023-04-10 17:12:40 +0800

    npm install supervallains

--- introToNode.js
+++ introToNode.js
@@ -2,3 +2,20 @@
 
 // fs.copyFileSync("source.txt", "destination.txt");
 
+
+// var superheroes = require('superheroes');
+
+
+// var superHeroNames = superheroes.random();
+
+// console.log(superHeroNames);
+
+
+var superheroes = require('superheroes');
+
+var superHeroList = superheroes.all;
+
+var superHeroNames = superheroes.random();
+
+console.log(superHeroList);
+console.log(superHeroNames);
