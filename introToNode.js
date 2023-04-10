1364ae9a1dd7ff5ecd1baa7fb67fbe2995c8ff3f
Regie Marzan <lonebeginner@gmail.com>
2023-04-10 17:22:36 +0800

    add supervillainsname & Names

--- introToNode.js
+++ introToNode.js
@@ -10,12 +10,15 @@
 
 // console.log(superHeroNames);
 
-
+const supervillains = require('supervillains');
 var superheroes = require('superheroes');
 
+var superVillains = supervillains.random();
+var superVillains = supervillains.all;
 var superHeroList = superheroes.all;
 
 var superHeroNames = superheroes.random();
 
 console.log(superHeroList);
 console.log(superHeroNames);
+console.log(superVillains);
